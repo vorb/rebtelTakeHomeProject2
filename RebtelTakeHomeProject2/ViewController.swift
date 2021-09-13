c38d609414715aa11426484233ac9a609134eb3a
Sebastian Mendez <sebastian.e.mendez@ica.se>
2021-09-13 16:04:31 +0200

    Initial Commit

---
+++ RebtelTakeHomeProject2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RebtelTakeHomeProject2
+//
+//  Created by Sebastian Mendez on 2021-09-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
